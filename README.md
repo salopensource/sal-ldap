a77431eff8e51ff9f7f7b45d230bb640ac3294f5
Graham Gilbert <graham@grahamgilbert.com>
2015-07-09 16:35:26 +0100

    first commit?

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sal-LDAP
+
+This image comprises of a Puppet Server is able to use LDAP authentication. There are many, many things that may need to be changed for your environment - this image works with my AD environment. As such, you may need to override ``settings.py`` completely. This image uses [django-auth-ldap](https://pythonhosted.org/django-auth-ldap/index.html_) to connect to LDAP, so please refer to the documentation there if you need a more custom configuration.
+
+## Usage
+
+This image extends (macadmins/sal)[https://registry.hub.docker.com/u/macadmins/sal/]. For full usage instructions for that image, see it's repository.
+
+## Environment variables
+
+* ``SAL_LDAP_SERVER_URI``: The URI of your LDAP server. Defaults to ``ldap://ldap``
+* ``SAL_LDAP_BIND_DN``: The distinguished name of your bind account.
+* ``SAL_LDAP_BIND_PASSWORD``: The password for your bind account.
+* ``SAL_LDAP_USER_SEARCH``: The search path for users.
+* ``SAL_LDAP_GROUP_SEARCH``: The search path for groups.
+* ``SAL_LDAP_REQUIRE_GROUP``: A group object to restrict login to.
+* ``SAL_LDAP_START_TLS``: Set to ``true`` to use TLS. Defaults to ``false``
+* ``SAL_LDAP_USER_ATTR``: The ldap attribute to identify the user. Defaults to ``sAMAccountName``
+* ``SAL_LDAP_LOGGING``: Set to ``true`` to enable logging of requests for debugging. Defaults to ``false``.
---
+++ settings.py
@@ -0,0 +1,71 @@
+# Django settings for sal project.
+from settings_import import ADMINS, TIME_ZONE, LANGUAGE_CODE, ALLOWED_HOSTS, DISPLAY_NAME, PLUGIN_ORDER
+from system_settings import *
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+
+AUTHENTICATION_BACKENDS = ('django_auth_ldap.backend.LDAPBackend','django.contrib.auth.backends.ModelBackend',)
+
+AUTH_LDAP_SERVER_URI = os.environ['SAL_LDAP_SERVER_URI']
+if os.environ['SAL_LDAP_START_TLS'].lower() == 'true':
+    AUTH_LDAP_START_TLS = True
+else:
+    AUTH_LDAP_START_TLS = False
+
+if os.environ['SAL_LDAP_BIND_DN']:
+    AUTH_LDAP_BIND_DN = os.environ['SAL_LDAP_BIND_DN']
+
+if os.environ['SAL_LDAP_BIND_PASSWORD']:
+    AUTH_LDAP_BIND_PASSWORD = os.environ['SAL_LDAP_BIND_PASSWORD']
+
+if os.environ['SAL_LDAP_USER_SEARCH']:
+    AUTH_LDAP_USER_SEARCH = LDAPSearch(os.environ['SAL_LDAP_USER_SEARCH'],
+    ldap.SCOPE_SUBTREE, "(%s=%(user)s)" % os.environ['SAL_LDAP_USER_ATTR'])
+
+
+
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+}
+
+if os.environ['SAL_LDAP_GROUP_SEARCH']:
+    AUTH_LDAP_GROUP_SEARCH = LDAPSearch(os.environ['SAL_LDAP_GROUP_SEARCH'],
+        ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)"
+    )
+    AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
+
+if os.environ['SAL_LDAP_REQUIRE_GROUP']:
+    AUTH_LDAP_REQUIRE_GROUP = os.environ['SAL_LDAP_REQUIRE_GROUP']
+
+if os.environ['SAL_LDAP_LOGGING'].lower() == 'true':
+    import logging
+
+    logger = logging.getLogger('django_auth_ldap')
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.DEBUG)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'db/sal.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# PG Database
+if os.environ.has_key('DB_PORT_5432_TCP_ADDR'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASS'],
+            'HOST': os.environ['DB_PORT_5432_TCP_ADDR'],
+            'PORT': os.environ['DB_PORT_5432_TCP_PORT'],
+        }
+    }
